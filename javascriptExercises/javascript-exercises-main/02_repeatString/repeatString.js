1bfc350438262b6daebea16ff1327bb3d21a4369
Antony <chipsmash11292@hotmail.com>
2022-11-04 10:40:46 +1100

    Finished tdd ex2

--- javascriptExercises/javascript-exercises-main/02_repeatString/repeatString.js
+++ javascriptExercises/javascript-exercises-main/02_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
+const repeatString = function(string, repeat) {
+    if (repeat < 0) {
+        return 'ERROR';
+    }
 
+    let repeatedString = "";
+    for (let i = 0; i < repeat; i++) {
+        repeatedString += string;
+    }
+    return repeatedString;
 };
 
 // Do not edit below this line
