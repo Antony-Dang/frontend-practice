0a7a119d260009f5745a1aa3df5b334551acd583
Antony <chipsmash11292@hotmail.com>
2022-11-04 11:02:58 +1100

    finished tdd exercise 4

--- javascriptExercises/javascript-exercises-main/04_removeFromArray/removeFromArray.js
+++ javascriptExercises/javascript-exercises-main/04_removeFromArray/removeFromArray.js
@@ -1,6 +1,23 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...args) {
+    console.log(arr);
+    console.log(args);
 
+    for (let i = 0; i < args.length; i++) {
+        console.log(args[i]);
+        arr = removeAllFromArray(arr, args[i]);
+    }
+    console.log(arr);
+    return arr;
 };
+
+function removeAllFromArray(arr, item) {
+    for (let i = arr.length; i >= 0; i --) {
+        if (arr[i] === item) {
+            arr.splice(i, 1);
+        }
+    }
+    return arr;
+}
 
 // Do not edit below this line
 module.exports = removeFromArray;
