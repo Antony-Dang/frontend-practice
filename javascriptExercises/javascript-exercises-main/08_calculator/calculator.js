9ff424622b907fb6d06354a911697b59a82f339b
Antony <chipsmash11292@hotmail.com>
2022-11-05 17:03:10 +1100

    Finished js ex8

--- javascriptExercises/javascript-exercises-main/08_calculator/calculator.js
+++ javascriptExercises/javascript-exercises-main/08_calculator/calculator.js
@@ -1,25 +1,33 @@
-const add = function() {
-	
+const add = function(n1, n2) {
+	return n1 + n2;
 };
 
-const subtract = function() {
-	
+const subtract = function(n1, n2) {
+	return n1 - n2;
 };
 
-const sum = function() {
-	
+const sum = function(arr1) {
+	return arr1.reduce((a, b) => a + b, 0)
 };
 
-const multiply = function() {
-
+const multiply = function(arr1) {
+  return arr1.reduce((a, b) => a * b, 1)
 };
 
-const power = function() {
-	
+const power = function(n1, n2) {
+	let total = 1;
+  for (let i = 0; i < n2; i++) {
+    total *= n1;
+  }
+  return total;
 };
 
-const factorial = function() {
-	
+const factorial = function(n1) {
+  if (n1 == 0) {
+    return 1;
+  }
+
+	return n1 * factorial(n1 - 1)
 };
 
 // Do not edit below this line
