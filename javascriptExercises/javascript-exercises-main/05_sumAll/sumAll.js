cbac7a23b5b9494a88ebddd5c99e7eb08e3005aa
Antony <chipsmash11292@hotmail.com>
2022-11-05 16:35:22 +1100

    Added implemntation of sumAll

--- javascriptExercises/javascript-exercises-main/05_sumAll/sumAll.js
+++ javascriptExercises/javascript-exercises-main/05_sumAll/sumAll.js
@@ -1,5 +1,22 @@
-const sumAll = function() {
+const sumAll = function(n1, n2) {
+    if (n1 < 0 || n2 < 0) {
+        return "ERROR";
+    }
+    if (typeof n1 != "number" || typeof n2 != "number") {
+        return "ERROR";
+    }
 
+    let total = 0
+    let i = n1;
+    let end = n2 + 1
+    if (n2 < n1) {
+        i = n2;
+        end = n1 + 1
+    }
+    for (; i < end; i++) {
+        total += i
+    }
+    return total
 };
 
 // Do not edit below this line
