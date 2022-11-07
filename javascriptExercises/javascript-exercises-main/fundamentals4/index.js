ddc3bc703c5cd35f5326a74891a3a67e22416528
Antony <chipsmash11292@hotmail.com>
2022-11-07 19:22:35 +1100

    added blue element before redp

--- javascriptExercises/javascript-exercises-main/fundamentals4/index.js
+++ javascriptExercises/javascript-exercises-main/fundamentals4/index.js
@@ -5,3 +5,8 @@ redParagraph.textContent = "Hey I'm Red!";
 redParagraph.style.color = 'red';
 
 container.appendChild(redParagraph);
+
+const blueText = document.createElement('h3');
+blueText.textContent = "I'm blue h3";
+blueText.style.color = 'blue';
+container.insertBefore(blueText, redParagraph);
