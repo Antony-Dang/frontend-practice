3e41e16219db2e0ddd01f8c913a54c516d32ceb1
Antony <chipsmash11292@hotmail.com>
2022-11-07 19:26:21 +1100

    added div inside div fund4

--- javascriptExercises/javascript-exercises-main/fundamentals4/index.js
+++ javascriptExercises/javascript-exercises-main/fundamentals4/index.js
@@ -10,3 +10,17 @@ const blueText = document.createElement('h3');
 blueText.textContent = "I'm blue h3";
 blueText.style.color = 'blue';
 container.insertBefore(blueText, redParagraph);
+
+const blackBox = document.createElement('div');
+blackBox.style.backgroundColor = 'pink';
+blackBox.style.border = 'black';
+
+const blackBoxh1 = document.createElement('h1');
+blackBoxh1.textContent = "I'm a div";
+const blackBoxPara = document.createElement('p');
+blackBoxPara.textContent = "Me TOO!";
+blackBox.appendChild(blackBoxh1);
+blackBox.appendChild(blackBoxPara);
+
+container.appendChild(blackBox);
+
