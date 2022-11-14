fcc33616a8a2a00a176f703832b812ed68dff8f9
Antony <chipsmash11292@hotmail.com>
2022-11-14 15:42:39 +1100

    Added calculator dir and finished fibonacci

--- javascriptExercises/javascript-exercises-main/10_fibonacci/fibonacci.js
+++ javascriptExercises/javascript-exercises-main/10_fibonacci/fibonacci.js
@@ -1,6 +1,34 @@
-const fibonacci = function() {
+let fibCalculated;
 
+const fibonacci = function(n) {
+    if (n < 0) {
+        return "OOPS";
+    }
+    if (isNaN(n)) {
+        return "OOPS";
+    }
+    if (typeof(n) == "string") {
+        n = parseInt(n);
+    }
+
+    fibCalculated = new Array(n + 1).fill(-1);
+    return fib(n);
 };
+
+function fib(num) {
+    if (num < 2) {
+        return num;
+    }
+    if (fibCalculated[num] != -1) {
+        return fibCalculated[num];
+    }
+    let fibNum = fib(num - 1) + fib(num - 2);
+    fibCalculated[num] = fibNum;
+    return fibNum;
+}
 
 // Do not edit below this line
 module.exports = fibonacci;
+
+
+fibonacci(4)
