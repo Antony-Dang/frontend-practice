f4d6cde7c74db8ce52b6eb5dd0e8579f1945079e
Antony <chipsmash11292@hotmail.com>
2022-11-14 15:57:34 +1100

    Finished findOldest

--- javascriptExercises/javascript-exercises-main/12_findTheOldest/findTheOldest.js
+++ javascriptExercises/javascript-exercises-main/12_findTheOldest/findTheOldest.js
@@ -1,5 +1,13 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    let oldestPerson;
+    let oldest = people.reduce((oldestAge, person) => {
+        let personAge = person["yearOfDeath"] - person["yearOfBirth"];
+        if (personAge > oldestAge) {
+            oldestPerson = person;
+            return personAge;
+        }
+    }, 0);
+    return oldestPerson;
 };
 
 // Do not edit below this line
