a731edcfeba255a6c65c320ad101cd761e300818
Antony <chipsmash11292@hotmail.com>
2022-11-19 11:02:40 +1100

    Added query for questions

--- Trivia/backend/api/DAO/questionsDAO.js
+++ Trivia/backend/api/DAO/questionsDAO.js
@@ -14,5 +14,24 @@ export default class questionsDAO {
           `Unable to establish a collection handle in restaurantsDAO: ${e} questions`,
         )
       }
+
+    }
+
+    static async getQuestions({
+      filters = null,
+      page = 0,
+      questionsPerPage,
+    } = {}) {
+      let query 
+      if (filters) {
+        if ("question" in filters) {
+          query = { $text: { $search: filters["question"]}}
+        } else if ("question_id" in filters) {
+          query = { $question_id: { $eq: filters["question_id"]}}
+        } else if ("answer_id" in filters) {
+          query = { $answer_id: { $eq: filters["answer_id"]}}
+        }
+        
+      }
     }
 }
